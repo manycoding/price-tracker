bfbff70ed72c99d6222779a98f3e0e16efd4388e
el_carbon <valery@spentapp.com>
2018-01-16 16:36:44 +0300

    Add requirements

--- pt/tasks.py
+++ pt/tasks.py
@@ -1,18 +1,7 @@
-# from celery.schedules import crontab
 from .models import Entry
 from .models import Price
 from price_tracker.celery import app
 import pt.fetcher as f
-
-
-# @app.on_after_configure.connect
-# def setup_periodic_tasks(sender, **kwargs):
-#     # Executes every day at 23:00
-#     sender.add_periodic_task(
-#         # crontab(hour=23, minute=00),
-#         crontab(),
-#         update_price_data.s(),
-#     )
 
 
 @app.task
