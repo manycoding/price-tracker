0469071de4a3e505783bdd275d04415bd7ed216b
manycoding <manycoding@users.noreply.github.com>
2018-02-15 15:47:32 +0300

    Set daily price fetch

--- celeryconfig.py
+++ celeryconfig.py
@@ -5,8 +5,8 @@ broker_url = os.environ['CLOUDAMQP_URL']
 beat_schedule = {
     'update_price_data': {
         'task': 'pt.tasks.update_price_data',
-        'schedule': crontab()
-        # 'schedule': crontab(hour=23, minute=00)
+        # 'schedule': crontab()
+        'schedule': crontab(hour=23, minute=00)
     },
 }
 broker_pool_limit = 1
