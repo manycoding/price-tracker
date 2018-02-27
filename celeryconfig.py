b80ff137fb350b706e636de5df0bf0e20434c43f
manycoding <manycoding@users.noreply.github.com>
2018-02-27 12:30:38 +0300

    Set different schedule for prod and local

--- celeryconfig.py
+++ celeryconfig.py
@@ -2,11 +2,12 @@ import os
 from celery.schedules import crontab
 
 broker_url = os.environ['CLOUDAMQP_URL']
+crontab_schedule = os.environ.get('CELERY_SCHEDULE', '*/1440')
+
 beat_schedule = {
     'update_price_data': {
         'task': 'pt.tasks.update_price_data',
-        # 'schedule': crontab()
-        'schedule': crontab(hour=23, minute=00)
+        'schedule': crontab(minute=crontab_schedule)
     },
 }
 broker_pool_limit = 1
